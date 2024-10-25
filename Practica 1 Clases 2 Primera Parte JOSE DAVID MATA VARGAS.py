14d27bf66b7b846447d6f444d1b0cd0c7998ed99
Jose David Mata Vargas <matavargasjosedavid@gmail.com>
2024-10-24 20:03:00 -0600

    Prueba

---
+++ Practica 1 Clases 2 Primera Parte JOSE DAVID MATA VARGAS.py
@@ -0,0 +1,15 @@
+Nombre= input("Ingrese su nombre: ")
+Apellido= input ("Ingrese su apellido: ")
+Edad= int (input("Ingrese su edad: "))
+print("El juego trata de adivinar el numero secreto del 1 al 10")
+Digito= int (input("Ingrese el numero secreto: "))
+Numsecreto= 6
+
+if (Digito == Numsecreto):
+    print("Felicidades acertaste el numero", Nombre +  Apellido )
+else:
+    print("Muchas gracias por participar")
+   
+     
+
+    
